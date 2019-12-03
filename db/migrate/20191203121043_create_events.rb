af08e7850876dcebcae57715d2ef8a8d30fa432b
leovanel <leovanel>
2019-12-03 13:30:48 +0100

    create event

---
+++ db/migrate/20191203121043_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.datetime :start_date
+      t.datetime :end_da
+      t.references :creator, foreign_key:{to_table::users} true
+
+      t.timestamps
+    end
+  end
+end
