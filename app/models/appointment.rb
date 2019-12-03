58cd75fc1ba6027ec6ea77d5b5aa96f7cfc6f8e9
leovanel <leovanel>
2019-12-04 00:07:34 +0100

    add relations to  events and fix appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,5 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :event
   has_many :product_appointments
+  has_many :products, through: :product_appointments
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User" 
   belongs_to :address
   belongs_to :event_type
+  has_many :comments
+  has_many :appointments 
+  has_many :users, through: :appointments  
+  has_many :product_events
+  has_many :products, through: :product_events
 end
