e4246d756b6c17de3f9e62dc953487434118ce17
leovanel <leovanel>
2019-12-04 00:20:59 +0100

    add dependent destroy to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User" 
   belongs_to :address
   belongs_to :event_type
-  has_many :comments
-  has_many :appointments 
+  has_many :comments, dependent: :destroy
+  has_many :appointments, dependent: :destroy 
   has_many :users, through: :appointments  
-  has_many :product_events
+  has_many :product_events, dependent: :destroy
   has_many :products, through: :product_events
 end
