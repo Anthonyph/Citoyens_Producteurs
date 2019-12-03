1065e34f725b5ef276b9fef08d36c2e456e3a65a
leovanel <leovanel>
2019-12-04 00:15:28 +0100

    add dependent destroy to appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  has_many :product_appointments
+  has_many :product_appointments, dependent: :destroy
   has_many :products, through: :product_appointments
 end
