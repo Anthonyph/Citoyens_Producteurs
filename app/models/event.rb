dd8e9bed0b3f6fbd609931a9e8e05b9bbe9a9992
leovanel <leovanel>
2019-12-04 18:28:43 +0100

    add validates to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,40 @@ class Event < ApplicationRecord
   has_many :users, through: :appointments  
   has_many :product_events, dependent: :destroy
   has_many :products, through: :product_events
+
+  validates :title,
+  presence: true,
+  length: {maximum: 30}
+
+  validates :description,
+  presence: true,
+  length: {maximum: 250}
+  
+
+  validates :start_date,
+  presence: true,
+  validate :event_past
+
+  validates :end_date,
+  presence: true,
+  validate :event_past, after_start_date
+  
+  private
+
+  def event_past
+   
+    if start_date < DateTime.now
+      errors.add(:start_date, "La date de départ de l'event ne peut pas etre dans le passé")
+    end
+  end
+
+  def after_start_date
+
+    if end_date < start_date
+      errors.add(:end_date, "La date de fin de l'évent doit être aprés le début de l'évent")
+
+    end
+  end
+
 end
+
