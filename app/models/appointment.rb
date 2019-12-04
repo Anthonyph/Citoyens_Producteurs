43ef967d2aad819c1939525102ac3eb43ec27d5f
leovanel <leovanel>
2019-12-04 16:43:33 +0100

    add validates to appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,36 @@ class Appointment < ApplicationRecord
   belongs_to :event
   has_many :product_appointments, dependent: :destroy
   has_many :products, through: :product_appointments
+
+  validates :start_date,
+  presence: true,
+  validate :event_past
+  validates :duration,
+  presence: true,:numericality => { :greater_than_or_equal_to => 1}
+  validate :valid_duration
+  validates :status, presence: true,
+  validates :points, presence: true
+
+  private
+  def event_past
+   
+    if start_date < DateTime.now
+      errors.add(:start_date, "La date de départ de l'event ne peut pas etre dans le passé")
+    end
+  end
+
+  def valid_duration
+    if (self.duration % 5) != 0
+      self.errors[:base] << "Number must be divisible by 5!"
+    end
+      
+  end
+
+  
+
+  
+
+  
+ 
+
 end
