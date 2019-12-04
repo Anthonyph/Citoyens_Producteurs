ea24803a066a07347045701fb3a4c89862b85057
Anthonyph <anthony.phim@gmail.com>
2019-12-04 17:03:48 +0100

    appointment mailer work in progress

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,4 +1,43 @@
 class AppointmentMailer < ApplicationMailer
   default from: 'no-reply@citoyens-producteurs.fr'
 
+  def new_appointment_user(appointment)
+    @apt = appointment
+    @apt_duration = @apt.duration
+    @apt_start_date = @apt.start_date
+    @user = @apt.user
+    @points = @apt.points
+    @user_email = @apt.user.email
+    @user_first_name = @apt.user.first_name
+    @user_last_name = @apt.user.last_name
+    @creator = @apt.event.creator
+    @creator_email = @creator.email
+    @creator_first_name = @creator.first_name
+    @creator_last_name = @creator.last_name
+
+    @url  = 'www.citoyens-producteurs.fr' 
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user_email, subject: 'Tu as rejoint un événement !') 
+  end
+
+  def new_appointment_creator(appointment)
+
+    @apt = appointment
+    @apt_duration = @apt.duration
+    @apt_start_date = @apt.start_date
+    @user = @apt.user
+    @user_email = @apt.user.email
+    @user_first_name = @apt.user.first_name
+    @user_last_name = @apt.user.last_name
+    @points = @apt.points
+    @creator = @apt.event.creator
+    @creator_email = @creator.email
+    @creator_first_name = @creator.first_name
+    @creator_last_name = @creator.last_name
+    
+    @url  = 'www.citoyens-producteurs.fr' 
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @creator_email, subject: 'Un nouveau citoyen s\'est inscrit à ton événement!') 
+  end
+
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,15 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  after_create :appointment_creation_user
+  after_create :appointment_creation_creator
+
+    def appointment_creation_user
+      AppointmentMailer.new_appointment_user(self).deliver_now
+    end
+
+    def appointment_creation_creator
+      AppointmentMailer.new_appointment_creator(self).deliver_now
+    end
+    
 end
