d3a4811ee079ae0a65ef2f41b2d0e5697576b934
leovanel <leovanel>
2019-12-05 18:13:36 +0100

    fix validates for seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,27 +18,19 @@ class Event < ApplicationRecord
     length: {maximum: 250}
   
 
-  validates :start_date, presence: true,  if: :event_past?
+  validates :start_date, presence: true, 
+  date: { after: Proc.new { Time.now }}
+  
 
-  validates :end_date, presence: true, if: :after_start_date?
-
+  validates :end_date, presence: true,
+  date: { after: :start_date }
 
   def event_creation_user
     EventMailer.new_event_user(self).deliver_now
   end
   
-  private
+  
 
-  def event_past?
-    start_date > DateTime.now
-    errors.add(:start_date, "La date de départ de l'event ne peut pas etre dans le passé")
-
-  end
-
-  def after_start_date?
-    end_date < start_date
-    errors.add(:end_date, "La date de fin de l'évent doit être aprés le début de l'évent")
-  end
 
 end
 
