1425478ebce01010603c8c8631fcd75921977be8
Anthonyph <anthony.phim@gmail.com>
2019-12-05 11:03:08 +0100

    FIX VALIDATE COMMIT

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,13 +5,11 @@ class Appointment < ApplicationRecord
   has_many :products, through: :product_appointments
 
 
-  validates :start_date,
-  presence: true,
-  validate :event_past
+  validates :start_date, presence: true, if: :event_past?
+
   validates :duration,
-  presence: true,:numericality => { :greater_than_or_equal_to => 1}
-  validate :valid_duration
-  validates :status, presence: true,
+  presence: true, numericality:  { :greater_than_or_equal_to => 1}, if: :valid_duration?
+  validates :status, presence: true
   validates :points, presence: true
 
   after_create :appointment_creation_user
@@ -26,18 +24,14 @@ class Appointment < ApplicationRecord
   end
 
   private
-  def event_past
-   
-    if start_date < DateTime.now
+  def event_past?
+      start_date > DateTime.now
       errors.add(:start_date, "La date de départ de l'event ne peut pas etre dans le passé")
-    end
   end
 
-  def valid_duration
-    if (self.duration % 5) != 0
-      self.errors[:base] << "Number must be divisible by 5!"
-    end
-      
+  def valid_duration?
+    self.duration % 5 == 0
+    self.errors[:base] << "Number must be divisible by 5!"
   end 
  
 
