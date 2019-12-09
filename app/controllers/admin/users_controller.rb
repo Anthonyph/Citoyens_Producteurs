899ed969f1843276c15529bcdf8f09d52d170918
leovanel <leovanel>
2019-12-09 16:08:40 +0100

    edit users  admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
 
   before_action :check_if_admin
-  before_action :user_detail, only: [:show,:create,:update,:destroy]
+  before_action :user_detail, only: [:show,:create,:update,:destroy,:edit]
 
   def index
     
@@ -22,19 +22,21 @@ class Admin::UsersController < ApplicationController
   end
   def edit        
   end
-  def update
-   
-    @users = User.all
-    
-      
-    user_params= params.require(:user).permit(:email,:description,:first_name,:is_admin)    
-      if @user_detail.update(user_params) # essaie de sauvegarder en base @gossip
+  def update 
+        
+    @store = @user_detail.store
+    @address= @user_detail.address
+    @event = Event.new(email: user_params[:email], first_name: user_params[:first_name], last_name:  user_params[:last_name], phone_number: user_params[:phone_number], is_admin: user_params[:is_admin])
+    params.require(:user).permit(:email,:first_name,:last_name,:phone_number,:is_admin,:place,:zip_code,:city,:sector,:store) 
+    if (@user_detail.update(email: user_params[:email], first_name: user_params[:first_name], last_name:  user_params[:last_name], phone_number: user_params[:phone_number], is_admin: user_params[:is_admin]))&& (@address.update(place: user_params[:place], zip_code: user_params[:zip_code], city:  user_params[:city], sector: user_params[:sector]))&&(@store.update(store: user_params[:place]
             
       redirect_to admin_user_path(@user_detail.id) # si ça marche, il redirige vers la page d'index du site
     else
       render :edit  # sinon, il render la view new (qui est celle sur laquelle on est déjà)
     end
+      
   end
+
   def destroy
     
     @user_detail.destroy 
@@ -44,6 +46,10 @@ class Admin::UsersController < ApplicationController
 
   private
 
+  def user_params
+    params.require(:user).permit(:email,:first_name,:last_name,:phone_number,:is_admin,:place,:zip_code,:city,:sector,:store)
+  end
+ 
   def check_if_admin
 
     if current_user == nil
