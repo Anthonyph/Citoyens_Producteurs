45a024cde354d6242a94a2652d0fa63cdf69f408
mariollet <mariollet@outlook.fr>
2019-12-10 14:42:19 +0100

    Add navbar on devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,11 @@ module ApplicationHelper
     !current_page?(controller: '/home', action: 'index') &&
     !current_page?(controller: '/home', action: 'community') &&
     !current_page?(controller: '/home', action: 'story') &&
+    !current_page?(controller: '/devise/registrations', action: 'new') &&
+    !current_page?(controller: '/devise/registrations', action: 'edit') &&
+    !current_page?(controller: '/devise/sessions', action: 'new') &&
+    !current_page?(controller: '/devise/passwords', action: 'new') &&
+    !current_page?(controller: '/devise/passwords', action: 'edit') &&
     !admin?
   end
 
