0c8858df7b81da40cc7d9c4fbb5cc2da21971666
leovanel <leovanel>
2019-12-10 19:27:25 +0100

    is_admin default

--- db/migrate/20191206135355_add_is_admin_to_user.rb
+++ db/migrate/20191206135355_add_is_admin_to_user.rb
@@ -1,9 +1,5 @@
 class AddIsAdminToUser < ActiveRecord::Migration[5.2]
   def change
-<<<<<<< HEAD
-    add_column :users, :is_admin, :boolean,default: false
-=======
     add_column :users, :is_admin, :boolean, default: false
->>>>>>> development
   end
 end
