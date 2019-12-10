63f77fcabccc4fa57048e88fdab26610c1c4aed7
leovanel <leovanel>
2019-12-10 14:14:15 +0100

    controller admin product create

--- app/controllers/admin/product_controller.rb
+++ app/controllers/admin/product_controller.rb
@@ -1,2 +1,54 @@
 class Admin::ProductController < ApplicationController
+  before_action :check_if_admin
+
+  def index
+
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+
+    @product = Product.new(name: product_params[:name])
+    if @product.save
+      flash[:success] = 'Product successfully created'
+      redirect_to @product
+    else
+      flash.now[:danger] = 'Something went wrong, please check your input'
+      render new_admin_product
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+
+  def check_if_admin
+
+    if current_user == nil
+      flash[:danger] = "Vous n'avez pas acces a cette page"
+      redirect_to '/'
+    elsif current_user.is_admin == false
+      flash[:danger] = "Vous n'avez pas acces a cette page"
+      redirect_to '/'    
+    end
+  end
 end
