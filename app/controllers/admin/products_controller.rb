882df570d9e70e7ddb88da3ec3c28ed2e549275d
leovanel <leovanel>
2019-12-10 16:38:06 +0100

    complete crud on products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -17,27 +17,37 @@ class Admin::ProductsController < ApplicationController
   def create
 
     @product = Product.new(name: product_params[:name])
-    if @product.save
+    if @product.save!
       flash[:success] = 'Product successfully created'
-      redirect_to @product
+      redirect_to '/admin/products'
     else
       flash.now[:danger] = 'Something went wrong, please check your input'
-      render new_admin_product
+      render new_admin_product_path
     end
 
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def update
-
+    
+  @product = Product.find(params[:id]) 
+    if @product.update(product_params)
+      flash[:success] = 'Product successfully updated'
+      redirect_to '/admin/products'
+    else
+      flash.now[:danger] = 'Something went wrong, please check your input'
+      render :edit
+    end
+      
   end
 
-  private
+  private  
 
   def product_params
+
     params.require(:product).permit(:name)
   end
 
