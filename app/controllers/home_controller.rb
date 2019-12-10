87cecdfd94ebaf33b14cb269fe65c5d3e337b7cf
mariollet <mariollet@outlook.fr>
2019-12-10 14:32:36 +0100

    remove sidebar on views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
+
   def index
   end
 
   def story
   end
+
+  def community
+  end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
 
   def show_sidebar
-    !current_page?(controller: '/home', action: 'index') && !admin?
+    !current_page?(controller: '/home', action: 'index') &&
+    !current_page?(controller: '/home', action: 'community') &&
+    !current_page?(controller: '/home', action: 'story') &&
+    !admin?
   end
 
   def admin?
