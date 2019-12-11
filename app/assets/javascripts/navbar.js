e0867b8bd44f80078ce747fb50167a02f9069616
mariollet <mariollet@outlook.fr>
2019-12-11 01:52:54 +0100

    change navbar color on Scroll

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -5,8 +5,10 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
     document.getElementById("navbar").style.backgroundColor = "rgba(255,255,255,1)";
+    document.getElementById("navbar").style.borderBottom = "3px solid #9F9B30";
   } else {
     document.getElementById("navbar").style.backgroundColor = "rgba(255,255,255,0.3)";
+    document.getElementById("navbar").style.borderBottom = "3px solid transparent";
   }
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
