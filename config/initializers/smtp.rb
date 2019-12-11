ef4c43e548524be626cddbaa475422f4d069e869
Anthony <53303288+Anthonyph@users.noreply.github.com>
2019-12-11 14:43:18 +0100

    Update smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
-  domain: 'yourdomain.com',
+  domain: 'citoyens-producteurs.herokuapp.com',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   authentication: :login,
