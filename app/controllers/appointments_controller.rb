c6d576ec36be19d99a7c93f525096292aec68072
leovanel <leovanel>
2019-12-13 00:31:51 +0100

    before_action  appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,7 @@
 class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
   before_action :set_event, only: [:index, :new, :edit, :create, :update, :destroy]
+  before_action :is_blank_phone_number?, , only: [:new, :create,:edit,:update] 
 
   
   # GET /events/:event_id/appointments
@@ -53,6 +54,12 @@ class AppointmentsController < ApplicationController
   def set_event
     @event = Event.find(params[:event_id])
   end
+
+  def is_blank_phone_number?  
+    if current_user.phone_number.blank?
+      flash[:danger] = "Attention! pour rejoindre un événement, tu dois d'abord renseigner ton numéro de téléphone!(seul le créateur et les autres participants y auront accès)"
+      redirect_to edit_user_registration_path
+    end  
 
   def appointment_params
     params.require(:appointment).permit(:start_date, :duration, :points, :user_id, :event_id)
