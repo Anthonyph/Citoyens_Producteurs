112e4c856a7ceafa2faaca0b4502df15dfbc07d3
mariollet <mariollet@outlook.fr>
2020-03-10 20:26:35 +0100

    Add avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_150721) do
+ActiveRecord::Schema.define(version: 2020_03_10_181315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_150721) do
     t.bigint "store_id"
     t.boolean "is_admin", default: false
     t.boolean "has_payed", default: false
+    t.string "avatar", default: "avatar_1.png"
     t.index ["address_id"], name: "index_users_on_address_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
